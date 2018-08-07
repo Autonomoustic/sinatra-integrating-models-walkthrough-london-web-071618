aad70aa647f7e13a5f67a7fb33ccce8391895d16
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-08-07 20:25:36 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
 
   post '/' do
     text_from_user = params[:user_text]
-
     erb :results
+    @what = "Number of Words: #{text_from_user.count}"
   end
+
+  
 end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,12 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+
+  
+end
